92be0255d0dceb9bec73652a94b9b5cdfc3f1720
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-13 17:01:51 +0300

    saved the favorites in an arrray to send them to the database

--- scripts/items.js
+++ scripts/items.js
@@ -2,49 +2,32 @@ document.getElementById("item_login_btn").onclick = function() {
     location.href = "./login_signup.html";
 };
 
-let mypromise = function getItems() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            let category_id = window.localStorage.getItem("category_id");
-            let url =
-                "http://127.0.0.1:8000/api/v1/user/items_by_category?id=" + category_id;
-            axios({
-                method: "get",
-                url: url,
-            }).then(function(response) {
-                console.log(response.data["items"].length);
-                for (let i = 0; i < response.data["items"].length; i++) {
-                    createItem(
-                        response.data["items"][i]["id"],
-                        response.data["items"][i]["name"],
-                        response.data["items"][i]["price"],
-                        response.data["items"][i]["image"]
-                    );
-                    console.log(response.data["items"][i]["name"]);
-                }
-            });
-            resolve();
-        }, 2000);
+function getItems() {
+    let category_id = window.localStorage.getItem("category_id");
+    let url =
+        "http://127.0.0.1:8000/api/v1/user/items_by_category?id=" + category_id;
+    axios({
+        method: "get",
+        url: url,
+    }).then(function(response) {
+        console.log(response.data["items"].length);
+        for (let i = 0; i < response.data["items"].length; i++) {
+            createItem(
+                response.data["items"][i]["id"],
+                response.data["items"][i]["name"],
+                response.data["items"][i]["price"],
+                response.data["items"][i]["image"]
+            );
+            console.log(response.data["items"][i]["name"]);
+        }
     });
-};
-
-mypromise().then((res) => {
-    console.log("KK");
-    const fav = document.getElementsByClassName("fav");
-    console.log(fav.length);
-    for (let i = 0; i < fav.length; i++) {
-        fav[i].toggleClass("press", 1000);
-        fav[i].addEventListener("click", function() {
-            window.localStorage.setItem("favorite_id", fav[i].id);
-        });
-    }
-});
+}
 
 var count = 0;
 let list = [];
+let fav_array = [];
 
 function createItem(id, name, price, image) {
-    count++;
     let item = document.createElement("div");
     item.setAttribute("id", id);
     item.setAttribute("class", "category_item");
@@ -72,11 +55,16 @@ function createItem(id, name, price, image) {
     let items = document.getElementsByClassName("items")[0];
     items.appendChild(item);
 
-    var item_obj = {
-        obj_id: id,
-        obj_name: name,
-        obj_fav: count,
-    };
-    list.push(item_obj);
-    localStorage.setItem("list", JSON.stringify(list));
+    document.getElementById(count).addEventListener("click", function() {
+        fav_array.push(id);
+        var item_obj = {
+            obj_id: id,
+            obj_name: name,
+            obj_fav: id,
+        };
+        list.push(item_obj);
+        localStorage.setItem("list", JSON.stringify(list));
+    });
+    count++;
 }
+getItems();
